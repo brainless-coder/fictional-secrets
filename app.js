1b3b8417442eada155b39f9ae48acd8dea08d54d
Arvind Prime <singhamish27@gmail.com>
2021-06-20 13:28:58 +0530

    Level-2 security added mongoose encryption

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+const port = 8081;
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/secretsDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+// This schema is just a JS object, and it works as long as we are not doing anything fancy with the schema
+// const userSchema = {
+//     email: String, 
+//     password: String
+// }; 
+
+// alsi mongoose schema define karne ka tarika
+const userSchema = new mongoose.Schema ({
+    email: String, 
+    password: String
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"] });
+
+// making a model or collection
+const User =  new mongoose.model("User", userSchema);
+
+
+app.get('/', function (req, res) {
+    res.render("home");
+});
+
+app.get('/login', function (req, res) {
+    res.render("login");
+});
+
+app.get('/register', function (req, res) {
+    res.render("register");
+});
+
+app.post('/register', function (req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save((err) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    });
+});
+
+app.post('/login', function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets");
+                }
+            }
+        }
+    })
+});
+
+
+app.listen(port, function() {
+    console.log(`Server is running on port ${port}`);
+});
